d28b5243b9fd901bf80d154ea8acb0c6acb479ad
Riza Nafis <31271087+ryuuzake@users.noreply.github.com>
2020-12-01 22:37:14 +0700

    Create Private Window (#16)
    * Create Private Window
    Fixes #14
    
    * Create Private Window
    Fixes #14
    
    * Change MyWindow to MyPage
    Fixes #14
    
    * Make Routing between Page
    Fixes #14
    
    Co-authored-by: rizabach <riza.bachtiar.rb@gmail.com>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Velacro.UIElements.Basic;
+using SquareCheck_desktop.Private;
+using System;
 
 namespace SquareCheck_desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MyWindow
     {
+        private MyPage loginPage, privatePage;
+
         public MainWindow()
         {
             InitializeComponent();
+            privatePage = new PrivatePage(GoToLoginPage);
+            GoToPrivatePage();
+        }
+
+        public void GoToPrivatePage()
+        {
+            mainFrame.Navigate(privatePage);
+        }
+
+        public void GoToLoginPage()
+        {
+            mainFrame.Navigate(loginPage);
         }
     }
 }
---
+++ Private/PrivatePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using Velacro.UIElements.Basic;
+
+namespace SquareCheck_desktop.Private
+{
+    /// <summary>
+    /// Interaction logic for PrivatePage.xaml
+    /// </summary>
+    public partial class PrivatePage : MyPage
+    {
+        public Action GoToLoginPage { get; }
+
+        public PrivatePage(Action goToLoginPage)
+        {
+            InitializeComponent();
+            this.KeepAlive = true;
+            GoToLoginPage = goToLoginPage;
+        }
+
+        private void logout_selected(object sender, RoutedEventArgs e)
+        {
+            GoToLoginPage();
+        }
+
+        private void matakuliah_selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mahasiswa_selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void home_selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
