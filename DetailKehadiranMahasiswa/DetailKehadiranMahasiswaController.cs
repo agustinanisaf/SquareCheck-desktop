6f91393be13d6491c00f02bdd0290fbc129f943b
agustinanisaf <agusteeenisaf@gmail.com>
2020-12-09 14:47:37 +0700

    Create Detail Kehadiran Mahasiswa Fixes #26

---
+++ DetailKehadiranMahasiswa/DetailKehadiranMahasiswaController.cs
@@ -0,0 +1,39 @@
+﻿using SquareCheck_desktop.Api;
+using SquareCheck_desktop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+
+namespace SquareCheck_desktop.DetailKehadiranMahasiswa
+{
+    class DetailKehadiranMahasiswaController : MyController
+    {
+        public DetailKehadiranMahasiswaController(IMyView _myView) : base(_myView)
+        { 
+        }
+
+        public async void getListDetailKehadiranMahasiswa(int subjectId) 
+        {
+            try
+            {
+                await ApiGenerator.GetListDetailKehadiranMahasiswa(viewShowListDetailKehadiranMahasiswa, subjectId);
+            }
+            catch (HttpRequestException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void viewShowListDetailKehadiranMahasiswa(HttpResponseBundle _response)
+        {
+            if (_response.getHttpResponseMessage().Content != null)
+            {
+                getView().callMethod("showListDetailKehadiranMahasiswa", _response.getParsedObject<APIResponse<List<AttendanceModel>>>().Data);
+            }
+        }
+    }
+}
---
+++ DetailKehadiranMahasiswa/DetailKehadiranMahasiswaPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using SquareCheck_desktop.DetailKehadiranMahasiswa;
+using SquareCheck_desktop.Api;
+using SquareCheck_desktop.Model;
+using System;
+using System.Collections.Generic;
+using Velacro.Api;
+using Velacro.UIElements.Basic;
+
+
+namespace SquareCheck_desktop.DetailKehadiranMahasiswa
+{
+    /// <summary>
+    /// Interaction logic for DetailKehadiranMahasiswaPage.xaml
+    /// </summary>
+    public partial class DetailKehadiranMahasiswaPage : MyPage
+    {
+        public DetailKehadiranMahasiswaPage()
+        {
+            InitializeComponent();
+            setController(new DetailKehadiranMahasiswaController(this));
+            getController().callMethod("getListDetailKehadiranMahasiswa", 2);
+        }
+
+        private void initUIElements()
+        {
+            getController().callMethod("getListDetailKehadiranMahasiswa");
+        }
+
+        public void showListDetailKehadiranMahasiswa(List<SubjectModel> subjects)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                icListDetailKehadiranMahasiswa.ItemsSource = DetailKehadiranMahasiswaContext.FromSubject(subjects);
+            });
+        }
+    }
+}
